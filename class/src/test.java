c52c144713cde7e365c71312d9e70b15f7504649
AsadujjamanAsif <147911379+AsadujjamanAsif@users.noreply.github.com>
2024-03-06 22:02:42 +0600

    first commit

---
+++ class/src/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args) {
+
+        // object declare
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "Asadujjaman Asif";
+        teacher1.gender = "Male";
+        teacher1.phone = 01315141301;
+
+        System.out.println("Name\t\t:" + teacher1.name);
+        System.out.println("Gender\t\t:" + teacher1.gender);
+        System.out.println("Phone Number\t:" + teacher1.phone);
+
+    }
+
+}
